ca2844fbd97cec32ef99cfd247c6df1a0688a664
Daniel Mytka <91204145+daniel-m17@users.noreply.github.com>
2021-09-27 03:28:19 +0300

    Add files via upload

---
+++ tEXT.js
@@ -0,0 +1,5 @@
+let text = 'Test';
+    let upperString = text.toUpperCase()  
+    let firstChar = upperString[0].toLowerCase() 
+    let restOfString = firstChar + upperString.slice(1) 
+    console.log(restOfString) 
